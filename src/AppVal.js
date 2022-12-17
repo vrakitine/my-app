efae2aba175bbc4daba9a112470ca07a5bddf5fc
val <val@natmatch.com>
2022-12-17 15:46:53 -0500

    Clean up

--- src/AppVal.js
+++ src/AppVal.js
@@ -16,33 +16,17 @@ function MyButton() {
   );
 }
 
-
-
 function AppVal() {
-  const products = [
-    { title: 'Cabbage', isFruit: false, id: 1 },
-    { title: 'Garlic', isFruit: false, id: 2 },
-    { title: 'Apple', isFruit: true, id: 3 },
-  ];
-  const listItems = products.map(product =>
-    <li
-      key={product.id}
-      style={{
-        color: product.isFruit ? 'magenta' : 'darkgreen'
-      }}
-    >
-      {product.title}
-    </li>
-  );
+
 
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Start <MyButton />
+          First <MyButton />
         </p>
         <p>
-        <ul>{listItems}</ul>
+          Second <MyButton />
         </p>
       </header>
     </div>
