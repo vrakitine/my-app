efca69fdab9ec693473c7c7a9f580e5499c8c2c5
val <val@natmatch.com>
2022-12-17 16:41:52 -0500

    Added {count, onClick}

--- src/AppVal.js
+++ src/AppVal.js
@@ -2,47 +2,45 @@ import './App.css';
 import { useState } from 'react';
 // Playing in https://beta.reactjs.org/learn 
 
-function MyButtonOne() {
-  const [count_1, setCount] = useState(0);
-
-  function handleClick() {
-    setCount(prevCount => prevCount + 1);
-  }
-
-  return (
-    <button onClick={handleClick}>
-      MyButtonOne Clicked {count_1} times
-    </button>
-  );
-}
-
-function MyButtonTwo() {
-  const [count_2, setCount] = useState(0);
-
-  function handleClick() {
-    setCount(prevCount => prevCount + 1);
-  }
-
-  return (
-    <button onClick={handleClick}>
-      MyButtonTwo Clicked {count_2} times
-    </button>
-  );
-}
 
 function AppVal() {
-  
+
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          <MyButtonOne />
+          <MyButtonOne count = {count} onClick={handleClick}/>
         </p>
         <p>
-          <MyButtonTwo />
+          <MyButtonTwo count = {count} onClick={handleClick}/>
         </p>
       </header>
     </div>
+  );
+}
+
+function MyButtonOne({count, onClick}) {
+
+  return (
+    <button onClick={onClick}>
+      MyButtonOne Clicked {count} times
+    </button>
+  );
+}
+
+function MyButtonTwo({count, onClick}) {
+
+  return (
+    <button onClick={onClick}>
+      MyButtonTwo Clicked {count} times
+    </button>
   );
 }
 
