42a8661e51a70473ab2eb541a77de0a2591260c5
val <val@natmatch.com>
2022-12-17 15:22:47 -0500

    Added listItems

--- src/AppVal.js
+++ src/AppVal.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function AppVal() {
+  const products = [
+    { title: 'Cabbage', id: 1 },
+    { title: 'Garlic', id: 2 },
+    { title: 'Apple', id: 3 },
+  ];
+  const listItems = products.map(product =>
+    <li key={product.id}>
+      {product.title}
+    </li>
+  );
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Start
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+        <ul>{listItems}</ul>
+        </p>
       </header>
     </div>
   );
