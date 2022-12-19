b42c99aa7aa4666bc68ef50e61e127c57fe3d601
val <val@natmatch.com>
2022-12-18 21:46:11 -0500

    Fixing

--- src/AppVal.js
+++ src/AppVal.js
@@ -1,39 +1,44 @@
 import './App.css';
 import { useState } from 'react';
+// Playing in https://beta.reactjs.org/learn 
 
 
 function AppVal() {
 
-  const [currentAction, setCurrentAction] = useState('Action_init');
+  const [currentAction, setAction] = useState('Action_init');
   //const [previousAction, setPreviousActione] = useState('Action_init');
   const [direction, setDirection] = useState('Direction_00');
 
   console.log('currentAction', currentAction); 
   //console.log(direction); 
 
-  function getCurrentAction(d) {
-    setDirection(d);
+  function getAction(d) {
+    console.log('Click!!!'); 
+    //setAction('Action_one');
+    console.log(d); 
+    //setDirection(d);
     /*
     console.log(d); 
     setDirection(d);
     console.log(direction);
     if(d === 'Direction_one'){
-      //return(setCurrentAction('Action_one'));
+      //return(setAction('Action_one'));
     }
     if(d === 'Direction_two'){
-      //return(setCurrentAction('Action_two'));
+      //return(setAction('Action_two'));
     } 
     */
   }
+
 
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          <MyButtonOne onClick={getCurrentAction('Direction_one')} action = {currentAction} dd = {direction}/>
+          <MyButtonOne onClick={getAction('Direction_one')} action = {currentAction} dd = {direction}/>
         </p>
         <p>
-          <MyButtonTwo onClick={getCurrentAction('Direction_two')} action = {currentAction} dd = {direction}/>
+          <MyButtonTwo onClick={getAction('Direction_two')} action = {currentAction} dd = {direction}/>
         </p>
       </header>
     </div>
